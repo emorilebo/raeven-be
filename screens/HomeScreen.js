451b08dec957b50c39a96d7cc565186dff2e28d7
emorilebo <emorylebo@gmail.com>
2022-07-07 12:05:44 +0100

    ADD hericons

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { View, Text, SafeAreaView, Image } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
+import { ChevronDownIcon } from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
   const navigation = useNavigation();
@@ -24,8 +25,11 @@ const HomeScreen = () => {
                 className='h-7 w-7 bg-gray-300mp-4 rounded-full'
             />
             <View>
-                <Text>Raeven Market</Text>
-                <Text>Current Location</Text>
+                <Text className="font-bold text-gray-400 text-xs">Raeven Market</Text>
+                <Text className="font-bold text-xl">
+                    Current Location
+                    <ChevronDownIcon size={20} color="#00CCBB"/>
+                </Text>
             </View>
         </View>
       </Text>
