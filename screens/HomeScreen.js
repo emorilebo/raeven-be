beb9d4060f719d7adc4d8f9742c06d1252c0c264
emorilebo <emorylebo@gmail.com>
2022-07-07 11:19:15 +0100

    ADD Navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,19 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView } from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import {useNavigation} from '@react-navigation/native'
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {
+      navigation.setOptions({
+        headerShown: false,
+      });
+    }, [])
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+      <Text className="text-red-500">HomeScreen</Text>
+    </SafeAreaView>
   )
 }
 
