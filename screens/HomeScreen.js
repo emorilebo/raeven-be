cee632ea89bfb115dd8ed39a31befd7ffb38e0ff
emorilebo <emorylebo@gmail.com>
2022-07-07 11:31:00 +0100

    ADD Navigation

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,20 +1,36 @@
-import { View, Text, SafeAreaView } from 'react-native'
-import React, { useLayoutEffect } from 'react'
-import {useNavigation} from '@react-navigation/native'
+import { View, Text, SafeAreaView, Image } from "react-native";
+import React, { useLayoutEffect } from "react";
+import { useNavigation } from "@react-navigation/native";
 
 const HomeScreen = () => {
-    const navigation = useNavigation();
+  const navigation = useNavigation();
 
-    useLayoutEffect(() => {
-      navigation.setOptions({
-        headerShown: false,
-      });
-    }, [])
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerShown: true,
+    });
+  }, []);
+
   return (
     <SafeAreaView>
-      <Text className="text-red-500">HomeScreen</Text>
+      <Text className="text-red-500">
+        {/* Header */}
+        
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+            <Image
+                source={{
+                    uri:'https://links.papareact.com/wru'
+                }}
+                className='h-7 w-7 bg-gray-300mp-4 rounded-full'
+            />
+            <View>
+                <Text>Raeven Market</Text>
+                <Text>Current Location</Text>
+            </View>
+        </View>
+      </Text>
     </SafeAreaView>
-  )
-}
+  );
+};
 
-export default HomeScreen
+export default HomeScreen;
