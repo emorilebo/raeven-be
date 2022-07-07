ea44116390fd93cdf85475e1215f6adcd0078bef
emorilebo <emorylebo@gmail.com>
2022-07-07 10:47:26 +0100

    UPDATE Tailwind

--- App.js
+++ App.js
@@ -1,20 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { TailwindProvider } from "tailwindcss-react-native";
+
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start!</Text>
+    <TailwindProvider>
+      <View >
+      <Text className="text-red-500">Open up App.js to start!</Text>
       <StatusBar style="auto" />
     </View>
+    </TailwindProvider>
+    
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
