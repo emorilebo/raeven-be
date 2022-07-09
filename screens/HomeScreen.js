5a358fa992c10ec2e91070ed60f1c1f4fe627d26
emorilebo <emorylebo@gmail.com>
2022-07-09 13:56:51 +0100

    Header update

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,19 +1,19 @@
-import { View, Text, SafeAreaView, Image } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { ChevronDownIcon, UserIcon } from 'react-native-heroicons/outline'
+import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from 'react-native-heroicons/outline'
 
 const HomeScreen = () => {
   const navigation = useNavigation();
 
   useLayoutEffect(() => {
     navigation.setOptions({
-      headerShown: true,
+      headerShown: false,
     });
   }, []);
 
   return (
-    <SafeAreaView>
+    <SafeAreaView className="bg-white pt-5">
       <Text className="text-red-500">
         {/* Header */}
         
@@ -24,14 +24,22 @@ const HomeScreen = () => {
                 }}
                 className='h-7 w-7 bg-gray-300mp-4 rounded-full'
             />
-            <View>
-                <Text className="font-bold text-gray-400 text-xs">Raeven Market</Text>
+            <View className="flex-1">
+                <Text className="font-bold text-gray-400 text-xs">Raeven Marketplace</Text>
                 <Text className="font-bold text-xl">
                     Current Location
                     <ChevronDownIcon size={20} color="#00CCBB"/>
                 </Text>
             </View>
             <UserIcon size={35} color="#00CCBB"/>
+        </View>
+        {/* Search */}
+        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+                <SearchIcon color="gray" size={20}/>
+                <TextInput placeholder="Search for goods and services" keyboardType="default"/>
+          </View>
+          <AdjustmentsIcon color="#00CCBB"/>
         </View>
       </Text>
     </SafeAreaView>
