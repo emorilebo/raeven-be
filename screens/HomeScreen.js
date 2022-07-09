30670173169f59783d9171913cbd52489e05ba97
emorilebo <emorylebo@gmail.com>
2022-07-09 14:01:00 +0100

    Header done

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import { View, Text, SafeAreaView, Image, TextInput } from "react-native";
+import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
 import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from 'react-native-heroicons/outline'
@@ -17,7 +17,7 @@ const HomeScreen = () => {
       <Text className="text-red-500">
         {/* Header */}
         
-        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
             <Image
                 source={{
                     uri:'https://links.papareact.com/wru'
@@ -34,13 +34,18 @@ const HomeScreen = () => {
             <UserIcon size={35} color="#00CCBB"/>
         </View>
         {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
           <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
                 <SearchIcon color="gray" size={20}/>
                 <TextInput placeholder="Search for goods and services" keyboardType="default"/>
           </View>
           <AdjustmentsIcon color="#00CCBB"/>
         </View>
+        {/* Body */}
+        <ScrollView>
+          {/* Categories */}
+          {/* Featured Row */}
+        </ScrollView>
       </Text>
     </SafeAreaView>
   );
