e6071c7702820f10e53a231b5f430d1670fa600c
emorilebo <emorylebo@gmail.com>
2022-07-09 14:17:33 +0100

    ADD CategoryCard

--- components/Categories.js
+++ components/Categories.js
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
+import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
+import CategoryCard from './CategoryCard'
 
 const Categories = () => {
   return (
-    <View>
+    <ScrollView contentContainerStyle={{paddingHorizontal:15, paddingTop:10,}} horizontal showsVerticalScrollIndicator={false}>
+        {/* CategoryCard */}
+        <CategoryCard/>
       <Text>Categories</Text>
-    </View>
+    </ScrollView>
   )
 }
 
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,7 +1,19 @@
-import { View, Text, SafeAreaView, Image, TextInput, ScrollView } from "react-native";
+import {
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TextInput,
+  ScrollView,
+} from "react-native";
 import React, { useLayoutEffect } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { AdjustmentsIcon, ChevronDownIcon, SearchIcon, UserIcon } from 'react-native-heroicons/outline'
+import {
+  AdjustmentsIcon,
+  ChevronDownIcon,
+  SearchIcon,
+  UserIcon,
+} from "react-native-heroicons/outline";
 import Categories from "../components/Categories";
 
 const HomeScreen = () => {
@@ -15,40 +27,45 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt-5">
-      
-        {/* Header */}
-        
-        <View className="flex-row pb-3 items-center mx-4 space-x-2 px-4">
-            <Image
-                source={{
-                    uri:'https://links.papareact.com/wru'
-                }}
-                className='h-7 w-7 bg-gray-300mp-4 rounded-full'
-            />
-            <View className="flex-1">
-                <Text className="font-bold text-gray-400 text-xs">Raeven Marketplace</Text>
-                <Text className="font-bold text-xl">
-                    Current Location
-                    <ChevronDownIcon size={20} color="#00CCBB"/>
-                </Text>
-            </View>
-            <UserIcon size={35} color="#00CCBB"/>
+      {/* Header */}
+      <View className="flex-row pb-3 items-center mx-4 space-x-2 ">
+        <Image
+          source={{
+            uri: "https://links.papareact.com/wru",
+          }}
+          className="h-7 w-7 bg-gray-300mp-4 rounded-full"
+        />
+        <View className="flex-1">
+          <Text className="font-bold text-gray-400 text-xs">
+            Raeven Marketplace
+          </Text>
+          <Text className="font-bold text-xl">
+            Current Location
+            <ChevronDownIcon size={20} color="#00CCBB" />
+          </Text>
         </View>
-        {/* Search */}
-        <View className="flex-row items-center space-x-2 pb-2 mx-4 px-4">
-          <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
-                <SearchIcon color="gray" size={20}/>
-                <TextInput placeholder="Search for goods and services" keyboardType="default"/>
-          </View>
-          <AdjustmentsIcon color="#00CCBB"/>
+        <UserIcon size={35} color="#00CCBB" />
+      </View>
+      {/* Search */}
+      <View className="flex-row items-center space-x-2 pb-2 mx-4">
+        <View className="flex-row flex-1 space-x-2 bg-gray-200 p-3">
+          <SearchIcon color="gray" size={20} />
+          <TextInput
+            placeholder="Search for goods and services"
+            keyboardType="default"
+          />
         </View>
-        {/* Body */}
-        <ScrollView className="bg-gray-100" contentContainerStyle={{paddingBottom: 100}}>
-          {/* Categories */}
-          <Categories/>
-          {/* Featured Row */}
-        </ScrollView>
-
+        <AdjustmentsIcon color="#00CCBB" />
+      </View>
+      {/* Body */}
+      <ScrollView
+        className="bg-gray-100"
+        contentContainerStyle={{ paddingBottom: 100 }}
+      >
+        {/* Categories */}
+        <Categories />
+        {/* Featured Row */}
+      </ScrollView>
     </SafeAreaView>
   );
 };
