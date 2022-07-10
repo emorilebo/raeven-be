3f5f599f8a065e52d3eadb933c65644515332d32
emorilebo <emorylebo@gmail.com>
2022-07-10 13:06:02 +0100

    UPDATE Sanity for category

--- sanity.js
+++ sanity.js
@@ -2,5 +2,16 @@ import sanityClient  from "@sanity/client";
 import imageUrlBuilder  from "@sanity/image-url";
 
 const client = sanityClient({
-    
-})
+    projectId: "1fv54rcn",
+    dataset: "production",
+    useCdn: true,
+    apiVersion: "2021-10-21",
+});
+
+const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
+
+//RUN THIS to add exception for localhost 3000 CORS policy
+//sanity cors add http://localhost:3000
+
+export default client;
