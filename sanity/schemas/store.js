9a836d7cfb2616715147dcc43776e4c29a2d0dd5
emorilebo <emorylebo@gmail.com>
2022-07-10 12:21:43 +0100

    UPDATE Sanity for category

--- sanity/schemas/store.js
+++ sanity/schemas/store.js
@@ -35,6 +35,29 @@ export default {
     type: "string",
     title: "Store Address",
     validation: (Rule)=>Rule.required(),
-   }
+   },
+   {
+    name: "rating",
+    type: "number",
+    title: "Enter a Rating from (1-5 Stars)",
+    validation: (Rule)=>
+      Rule.required()
+        .min(1)
+        .max(5)
+        .error("Please enter a value between 1 and 5"),
+   },
+   {
+    name: "type",
+    title: "Category",
+    validation: (Rule)=> Rule.required(),
+    type: "reference",
+    to:[{type: "category"}],
+   },
+   {
+    name: "dishes",
+    type: "array",
+    title: "Dishes",
+    of:[{type: "reference", to: [{type: "dish"}] }],
+   },
   ],
 }
